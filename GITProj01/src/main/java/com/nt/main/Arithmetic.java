b83a463e1b291d3b24f12e65ff1907c275a48685
dibya <dibya@Dibyaranjan>
2025-10-23 23:35:51 +0530

    gf

--- GITProj01/src/main/java/com/nt/main/Arithmetic.java
+++ GITProj01/src/main/java/com/nt/main/Arithmetic.java
@@ -8,17 +8,8 @@ public class Arithmetic {
 	public  double sub(double x,double y) {
 
 		System.out.println("Write logic");
-<<<<<<< HEAD
-		System.out.println(x-y);
-=======
-<<<<<<< HEAD
 		System.out.println("njkzbhjb");
-		return x*y;
-=======
-		System.out.println(x+y);
->>>>>>> branch 'master' of https://github.com/Dibya-Ranjan-Routaray/FSTools501Repo.git
-		return x+y;
->>>>>>> branch 'master' of https://github.com/Dibya-Ranjan-Routaray/FSTools501Repo.git
+		return x-y;
 	}
 	
 }
